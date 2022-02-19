6fb864c1c5dcebc20fc38b592642d4d19e571604
Jean Christophe Gomis <devdujour@yahoo.com>
2022-02-19 10:02:38 +0100

    mise en place de la regex pour la validation du pseudo

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
 //Récupération des inputs
 
 const inputs = document.querySelectorAll('input[type = "text"], input[type = "password"], input[type = "email"');
+// Fonction générale pour toute la logique d'affichage des erreurs;
+
+let pseudo, email, password, confirm;
+
+const errorDisplay = (tag, message, valid) => {
+      const container = document.querySelector("." + tag + "-container")//récupération des tag de façon dynamique;
+      const span = document.querySelector('.' + tag + '-container >span ');
+
+      if (!valid) {
+            container.classList.add('error');
+            span.textContent = message;
+      } else {
+            container.classList.remove('error');
+            span.textContent = message
+      }
+
+}
 
 // Je crée un évenement pour tous les inputs
 // je crée des fonctions checker pour vérifier les données
 const pseudoChecker = (value) => {
-      const pseudoContainer = document.querySelector('.pseudo-container');
-
-      const errorDisplay = document.querySelector('.pseudo-container > span');
 
       if (value.length > 0 && (value.length < 3 || value.length > 20)) {
-
-            pseudoContainer.classList.add('error');
-
-            errorDisplay.textContent = "Le pseudo doit contenir entre 3 et 20 caractéres !"
+            errorDisplay("pseudo", "Le pseudo doit faire entre 3 et 20 caractéres");
+            // Si les données sont invalides
+            pseudo = null;
 
       } else if (!value.match(/^[a-zA-z0-9_.-]*$/)) {
-            pseudoContainer.classList.add('error');
-            errorDisplay.textContent = " Le pseudo ne dois pas contenir de caractéres spéciaux"
+            errorDisplay("pseudo", " Le pseudo ne doit pas contenir de caractéres spéciaux");
+            pseudo = null;
       } else {
-            pseudoContainer.classList.remove('error');
-            errorDisplay.textContent = "";
+            errorDisplay("pseudo", "", true)
+            // Si les données sont valides 
+            pseudo = value;
       }
 
 };
